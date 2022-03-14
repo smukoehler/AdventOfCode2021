c97b1f8b9d87dfadfc5878fd8aafec708a60c90b
Sarah Koehler <skoehler@berkeley.edu>
2022-03-14 11:04:47 -0700

    Finally got Dijkstra's algorithm right!

--- day15/main2.cc
+++ day15/main2.cc
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <limits>
 #include <string>
+#include <utility>
 #include <vector>
 
 const int kRepeats = 5;
@@ -44,12 +45,12 @@ int** GetFullRiskMap(int** tile, int num_rows, int num_cols) {
     return fullmap;
 }
 
-long** InitializeDistanceMap(int num_rows, int num_cols) {
-    long** map = new long*[num_rows];
+int** InitializeDistanceMap(int num_rows, int num_cols) {
+    int** map = new int*[num_rows];
     for (int row = 0; row < num_rows; ++row) {
-        map[row] = new long[num_cols];
+        map[row] = new int[num_cols];
         for (int col = 0; col < num_cols; ++col) {
-            map[row][col] = std::numeric_limits<long>::max();
+            map[row][col] = std::numeric_limits<int>::max();
         }
     }
     // The first node is special.
@@ -152,78 +153,121 @@ bool AreAllNodesVisited(bool** visited_nodes, int num_rows, int num_cols) {
     return check;
 }
 
-void RunDijkstra(long** distance_map, bool** visited_nodes, int** fullmap, int* current_row, int* current_col, int num_rows, int num_cols) {
-    long current_cost = distance_map[*current_row][*current_col];
-    std::cout << "Current node at: (" << *current_row << " , " << *current_col << ")" << std::endl;
-    std::cout << "Curent cost: " << current_cost << std::endl;
-    
-    // Visit the neighbors of the current node.
-    // Left neighbor.
-    if (*current_col > 0) {
-        int neighbor_row = *current_row;
-        int neighbor_col = *current_col - 1;
-        long distance = (long)fullmap[neighbor_row][neighbor_col] + current_cost;
-        if (distance < distance_map[neighbor_row][neighbor_col]) {
-            distance_map[neighbor_row][neighbor_col] = distance;
-        }
-    }
-    // Right neighbor.
-    if (*current_col < num_cols - 1) {
-        int neighbor_row = *current_row;
-        int neighbor_col = *current_col + 1;
-        long distance = (long)fullmap[neighbor_row][neighbor_col] + current_cost;
-        if (distance < distance_map[neighbor_row][neighbor_col]) {
-            distance_map[neighbor_row][neighbor_col] = distance;
-        }
-    }
-    // Up neighbor.
-    if (*current_row > 0) {
-        int neighbor_row = *current_row - 1;
-        int neighbor_col = *current_col;
-        long distance = (long)fullmap[neighbor_row][neighbor_col] + current_cost;
-        if (distance < distance_map[neighbor_row][neighbor_col]) {
-            distance_map[neighbor_row][neighbor_col] = distance;
-        }
-    }
-    // Up neighbor.
-    if (*current_row < num_rows - 1) {
-        int neighbor_row = *current_row + 1;
-        int neighbor_col = *current_col;
-        long distance = (long)fullmap[neighbor_row][neighbor_col] + current_cost;
-        if (distance < distance_map[neighbor_row][neighbor_col]) {
-            distance_map[neighbor_row][neighbor_col] = distance;
-        }
-    }
-
-    // Mark the current node as visited.
-    visited_nodes[*current_row][*current_col] = true;
-
-    // Choose the next node to visit.
-    int next_row = *current_row;
-    int next_col = *current_col;
+// It turns out this is not a good approach because then you are not keeping track of the shortest possible path. It does appear to work
+// in all cases where bellman's principle of optimality applies.
+void ChooseChronologicalNext(int& current_row, int& current_col) {
+    int next_row = current_row;
+    int next_col = current_col;
     // If on the diagonal, start at the top of the rows in the next col.
-    if (*current_row == *current_col) {
+    if (current_row == current_col) {
         next_row = 0;
         next_col++;
     }
     // If row < col, proceed down the col until you almost get to the diagonal. Then jump to col 0.
-    if (*current_row < *current_col) {
-        if (*current_row == *current_col - 1) {
+    if (current_row < current_col) {
+        if (current_row == current_col - 1) {
             next_row++;
             next_col = 0;
         } else {
             next_row++;
-            next_col = *current_col;
+            next_col = current_col;
         }
     }
-    if (*current_col < *current_row) {
-        next_row = *current_row;
+    if (current_col < current_row) {
+        next_row = current_row;
         next_col++;
     }
     // std::cout << "Next node at: (" << next_row << " , " << next_col << ")" << std::endl;
     // RunDijkstra(distance_map, visited_nodes, fullmap, next_row, next_col, num_rows, num_cols);
-    *current_row = next_row;
-    *current_col = next_col;
+    current_row = next_row;
+    current_col = next_col;
+}
+
+// Always choose the minimum distance unvisited node so that you are always keeping track of the shortest path.
+void ChooseMinimumDistance(int** distance_map, std::vector<std::pair<int, int>>&unvisited_nodes, int& current_row, int& current_col) {
+    int next_row = unvisited_nodes[0].first;
+    int next_col = unvisited_nodes[0].second;
+    for (auto& node : unvisited_nodes) {
+        if(distance_map[node.first][node.second] < distance_map[next_row][next_col]) {
+            next_row = node.first;
+            next_col = node.second;
+        }
+    }
+    current_row = next_row;
+    current_col = next_col;
+
+    // Remove the selected node from unvisited nodes.
+    std::vector<std::pair<int, int>> reduced_nodes;
+    for (auto& node : unvisited_nodes) { 
+        if (node.first != next_row || node.second != next_col) {
+            reduced_nodes.push_back(node);
+        }
+    }
+    unvisited_nodes = reduced_nodes;
+    return;
+}
+
+void RunDijkstra(int** distance_map, bool** visited_nodes, std::vector<std::pair<int, int>>& unvisited_nodes, int** fullmap, int& current_row, int& current_col, int num_rows, int num_cols) {
+    int current_cost = distance_map[current_row][current_col];
+    std::cout << "Current node at: (" << current_row << " , " << current_col << ")" << std::endl;
+    std::cout << "Current cost: " << current_cost << std::endl;
+    
+    // Visit the neighbors of the current node.
+    // Left neighbor.
+    if (current_col > 0) {
+        int neighbor_row = current_row;
+        int neighbor_col = current_col - 1;
+        int distance = (int)fullmap[neighbor_row][neighbor_col] + current_cost;
+        if (distance < distance_map[neighbor_row][neighbor_col]) {
+            distance_map[neighbor_row][neighbor_col] = distance;
+        }
+        if (visited_nodes[neighbor_row][neighbor_col] == false) {
+            unvisited_nodes.push_back(std::make_pair(neighbor_row, neighbor_col));
+        }
+    }
+    // Right neighbor.
+    if (current_col < num_cols - 1) {
+        int neighbor_row = current_row;
+        int neighbor_col = current_col + 1;
+        int distance = (int)fullmap[neighbor_row][neighbor_col] + current_cost;
+        if (distance < distance_map[neighbor_row][neighbor_col]) {
+            distance_map[neighbor_row][neighbor_col] = distance;
+        }
+        if (visited_nodes[neighbor_row][neighbor_col] == false) {
+            unvisited_nodes.push_back(std::make_pair(neighbor_row, neighbor_col));
+        }
+    }
+    // Up neighbor.
+    if (current_row > 0) {
+        int neighbor_row = current_row - 1;
+        int neighbor_col = current_col;
+        int distance = (int)fullmap[neighbor_row][neighbor_col] + current_cost;
+        if (distance < distance_map[neighbor_row][neighbor_col]) {
+            distance_map[neighbor_row][neighbor_col] = distance;
+        }
+        if (visited_nodes[neighbor_row][neighbor_col] == false) {
+            unvisited_nodes.push_back(std::make_pair(neighbor_row, neighbor_col));
+        }
+    }
+    // Up neighbor.
+    if (current_row < num_rows - 1) {
+        int neighbor_row = current_row + 1;
+        int neighbor_col = current_col;
+        int distance = (int)fullmap[neighbor_row][neighbor_col] + current_cost;
+        if (distance < distance_map[neighbor_row][neighbor_col]) {
+            distance_map[neighbor_row][neighbor_col] = distance;
+        }
+        if (visited_nodes[neighbor_row][neighbor_col] == false) {
+            unvisited_nodes.push_back(std::make_pair(neighbor_row, neighbor_col));
+        }
+    }
+
+    // Mark the current node as visited.
+    visited_nodes[current_row][current_col] = true;
+
+    // Choose the next node to visit.
+    // ChooseChronologicalNext(current_row, curent_col);
+    ChooseMinimumDistance(distance_map, unvisited_nodes, current_row, current_col);
 }
 
 
@@ -256,14 +300,17 @@ int main()
     // PrintRiskMap(costtogo, num_rows * kRepeats, num_cols * kRepeats);
     // std::cout << MinCost(fullmap, costtogo, num_rows * kRepeats - 1, num_cols * kRepeats - 1) << std::endl;
 
+    // List of all the nodes that have a non-infinite distance, but are yet unvisited.
+    std::vector<std::pair<int, int>> unvisited_nodes;
     bool** visited_nodes = InitializeVisitedNodes(num_rows * kRepeats, num_cols * kRepeats);
-    long** distance_map = InitializeDistanceMap(num_rows * kRepeats, num_cols * kRepeats);
+    int** distance_map = InitializeDistanceMap(num_rows * kRepeats, num_cols * kRepeats);
     // PrintMap(distance_map, num_rows, num_cols);
 
     int row = 0;
     int col = 0;
     while (!AreAllNodesVisited(visited_nodes, num_rows * kRepeats, num_cols * kRepeats)) {
-        RunDijkstra(distance_map, visited_nodes, fullmap, &row, &col, num_rows * kRepeats, num_cols * kRepeats);
+        RunDijkstra(distance_map, visited_nodes, unvisited_nodes, fullmap, row, col, num_rows * kRepeats, num_cols * kRepeats);
+        std::cout << "Next node at: (" << row << " , " << col << ")" << std::endl;
     }
     // PrintMap(distance_map, num_rows, num_cols);
 
@@ -289,11 +336,6 @@ int main()
         delete [] distance_map[row];
     }
     delete [] distance_map;
-
-    // for (int row = 0; row < num_rows * kRepeats; ++row) {
-    //     delete [] costtogo[row];
-    // }
-    // delete [] costtogo;
 
     return 0;
 }
